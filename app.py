1b01bf65827506499449ee9f7f860b78a533c9ec
jordanbradburne <85847344+jordanbradburne@users.noreply.github.com>
2021-08-21 15:11:39 -0500

    Create the Welcome Route

--- app.py
+++ app.py
@@ -1,26 +1,44 @@
-# Create a New Python File and Import the Flask Dependency
-from flask import Flask
+# Set Up the Flask Weather App
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-# Create a New Flask App Instance
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set Up the Database
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+# Set Up Flask
 app = Flask(__name__)
 
-# Create Flask Routes
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
-# Think of some simple code from which you could create a route. 
-# Then try to create a new route implementing that logic.
-# comment out code above to run app2
-app2 = Flask(__name__)
-@app2.route('/')
-def name():
-    return 'My name is Jordan'
+# When creating routes, we follow the naming convention /api/v1.0/ followed by the name of the route. 
+# This convention signifies that this is version 1 of our application. 
+# This line can be updated to support future versions of the app as well.
 
-
-# Run a Flask App
-# Run this in command line in terminal
-#   export FLASK_APP=app.py
-#   flask run
-# Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
-# Copy and paste the localhost address into the web browser. 
+# export FLASK_APP=app.py
+# flask run
+# http://127.0.0.1:5000/
